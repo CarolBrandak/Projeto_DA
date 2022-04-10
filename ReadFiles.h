095787035a558639d24eee85ad5eeb75a4ebfaf7
lmmp77 <lmmp789@gmail.com>
2022-04-11 00:16:35 +0100

    Adicionar ficheiros .txt

---
+++ ReadFiles.h
@@ -0,0 +1,10 @@
+#include <fstream>
+#include <string>
+#include <vector>
+
+#ifndef UNTITLED_READFILES_H
+#define UNTITLED_READFILES_H
+
+void readLine(std::string fileName, std::string lineName,  )
+
+#endif //UNTITLED_READFILES_H
